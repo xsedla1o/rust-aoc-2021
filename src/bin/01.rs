40e4c9664fd6da560ced99aacc9fbfda6a6cc6f3
OndrejSedlacek <xsedla1o@stud.fit.vutbr.cz>
2023-05-24 14:42:32 +0200

    Solved day 1.

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,73 @@
+use std::{collections::VecDeque};
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let lines = input.lines();
+    let mut first = true;
+    let mut curr_depth: u32 = 0;
+    let mut new_depth: u32;
+    let mut decrease_cnt: u32 = 0;
+    for line in lines {
+        if first {
+            curr_depth = line.parse::<u32>().unwrap();
+            first = false;
+        } else {
+            // print!("{}\n", curr_depth);
+            new_depth = line.parse::<u32>().unwrap();
+            if new_depth > curr_depth {
+                decrease_cnt += 1;
+            }
+            curr_depth = new_depth;
+        }
+    }
+    Some(decrease_cnt)
+}
+
+pub fn print_deque(deque: &VecDeque<u32>) {
+    for item in deque.iter() {
+        print!("{}, ", item);
+    }
+    print!("\n");
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let lines = input.lines();
+    let mut deque: VecDeque<u32> = VecDeque::with_capacity(4);
+    let mut depth: u32;
+    let mut decrease_cnt: u32 = 0;
+    for line in lines {
+        if deque.len() < 3 {
+            depth = line.parse::<u32>().unwrap();
+            deque.push_back(depth);
+            // println!("{}", depth);
+        } else {
+            depth = line.parse::<u32>().unwrap();
+            deque.push_back(depth);
+            // print_deque(&deque);
+            
+            let mut curr_sum = 0;
+            for i in 0..3 {
+                curr_sum += deque.get(i).unwrap();
+                // println!("- {}", deque.get(i).unwrap());
+            }
+            // println!("->{}", curr_sum);
+
+            let mut new_sum = 0;
+            for i in 1..4 {
+                new_sum += deque.get(i).unwrap();
+                // println!("- {}", deque.get(i).unwrap());
+            }
+            // println!("->{}", new_sum);
+            
+            if new_sum > curr_sum {
+                decrease_cnt += 1;
+                // println!("Increase")
+            }
+
+            deque.pop_front();
+        }
+        // println!();
+    }
+    Some(decrease_cnt)
 }
 
 fn main() {
@@ -19,12 +83,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(7));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(5));
     }
 }
